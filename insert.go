b2f3a1f28a9c43a69010dfdef2f36d8cbce47190
Charlie Mathews <charlie@tovala.com>
2020-12-02 17:11:50 -0600

    Add InsertMany()

--- insert.go
+++ insert.go
@@ -15,17 +15,15 @@ func Insert(db *sqlx.DB, table string, model interface{}, excludedFields []strin
 	return db.NamedQuery(sql, model)
 }
 
-// Maybe go generics will make InsertMany feasible?
+func InsertManyQuery(db *sqlx.DB, table string, model interface{}, excludedFields []string) string {
+	fields := Fields(model)
+	filteredFields := helpers.Filter(fields, excludedFields)
+	formattedFields := strings.Join(helpers.PrependStrings(filteredFields, ":"), ", ")
+	sql := "INSERT INTO " + table + " (" + strings.Join(helpers.WrapStrings(filteredFields, "\""), ", ") + ") VALUES (" + formattedFields + ")"
+	return sql
+}
 
-// func InsertManyQuery(db *sqlx.DB, table string, model interface{}, excludedFields []string) string {
-// 	fields := Fields(model)
-// 	filteredFields := helpers.Filter(fields, excludedFields)
-// 	formattedFields := strings.Join(helpers.PrependStrings(filteredFields, ":"), ", ")
-// 	sql := "INSERT INTO " + table + " (" + strings.Join(helpers.WrapStrings(filteredFields, "\""), ", ") + ") VALUES (" + formattedFields + ")"
-// 	return sql
-// }
-
-// func InsertMany(db *sqlx.DB, table string, models []interface{}, excludedFields []string) (*sqlx.Rows, error) {
-// 	sql := InsertManyQuery(db, table, models, excludedFields)
-// 	return db.NamedQuery(sql, models)
-// }
+func InsertMany(db *sqlx.DB, table string, models []interface{}, excludedFields []string) (*sqlx.Rows, error) {
+	sql := InsertManyQuery(db, table, models, excludedFields)
+	return db.NamedQuery(sql, models)
+}
